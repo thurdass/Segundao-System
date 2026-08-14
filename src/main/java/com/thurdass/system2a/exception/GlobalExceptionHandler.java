e422d9bc3d5ca99a2691ed1f2149a8fd1f82ca68
ThurDas <va4591073@gmail.com>
2026-08-13 22:29:19 -0300

    style: order logging imports

--- src/main/java/com/thurdass/system2a/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thurdass/system2a/exception/GlobalExceptionHandler.java
@@ -2,12 +2,12 @@ package com.thurdass.system2a.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
